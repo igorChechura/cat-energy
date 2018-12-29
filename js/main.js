ee983f97cd5cf166bf1a4e4ea3b7ec809680d7b7
Igor Chechura <igorchechura@gmail.com>
2018-12-29 12:38:00 +0300

    Скрипт для оживления навигации на мобильном

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 var navToggle = document.querySelector('.nav-toggle');
 var mainNav = document.querySelector('.main-nav');
+
+navToggle.classList.remove('nav-toggle--no-js');
+mainNav.classList.add('main-nav--closed');
+
+navToggle.addEventListener('click', function(evt) {
+	evt.preventDefault();
+
+	this.classList.toggle('nav-toggle--opened');
+	mainNav.classList.toggle('main-nav--closed');
+});
